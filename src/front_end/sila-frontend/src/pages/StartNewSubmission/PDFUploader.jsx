662af1a74f69fa784f925ccf7a880c03e1dac146
DonghongZhuang <50001468+DonghongZhuang@users.noreply.github.com>
2024-04-27 14:45:22 +1000

    change upload holder message

--- src/front_end/sila-frontend/src/pages/StartNewSubmission/PDFUploader.jsx
+++ src/front_end/sila-frontend/src/pages/StartNewSubmission/PDFUploader.jsx
@@ -20,7 +20,7 @@ const PDFUploader = ({ onFileListChange, onFileUploaded }) => {
         onChange(info) {
             if (info.file.status === 'done') {
                 message.success(`${info.file.name} file uploaded successfully.`);
-                onFileUploaded(info.file.response); // Assuming the response includes the file path
+                onFileUploaded(info.file.response); 
             } else if (info.file.status === 'error') {
                 message.error(`${info.file.name} file upload failed.`);
             }
@@ -38,7 +38,8 @@ const PDFUploader = ({ onFileListChange, onFileUploaded }) => {
             <p className="ant-upload-drag-icon">
                 <InboxOutlined />
             </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-text">Please upload the anonymized manuscript first.</p>
+            <p className="ant-upload-text">Then all other files.</p>
         </Dragger>
     );
 };
