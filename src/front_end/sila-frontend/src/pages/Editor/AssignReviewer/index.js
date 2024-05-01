4ee96df9944fbda8715139433ea63ed8b5114357
Yizhi Liao <liaoyizhi23@163.com>
2024-05-01 18:06:43 +1000

    add FormModal

--- src/front_end/sila-frontend/src/pages/Editor/AssignReviewer/index.js
+++ src/front_end/sila-frontend/src/pages/Editor/AssignReviewer/index.js
@@ -8,6 +8,7 @@ import { Link, useNavigate } from "react-router-dom"
 import { ArticleStatus } from "../../../utils/status"
 import FormModal from "../FormModal"
 import { article_List_API } from "../../../apis/article"
+import { http } from "../../../utils"
 
 const AssignReviewer = ()=>{
     const navigate = useNavigate()
@@ -15,6 +16,8 @@ const AssignReviewer = ()=>{
     const [articleData, setArticleData] = useState(null);
     //get articles for review
     const [articles, setArticles] = useState([])
+    //get all reviewers
+    const [reviewers, setReviewers] = useState([])
 
     useEffect(() => {
       async function get_articles_by_author(id) {
@@ -32,13 +35,23 @@ const AssignReviewer = ()=>{
       get_articles_by_author(localStorage.getItem('id'));
     }, []);
 
+    function formatDate(dateString) {
+      const date = new Date(dateString);
+      return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+    }
+
     const showModal = async (article_id) => {
         
         try {
             console.log(article_id)
             // const data = await fetchArticleData(article_id);
-            // setArticleData({"title":"article1","content":"content1"}); 
-            // setIsModalOpen(true); 
+            const reviewers = await fetchAllReviewers(localStorage.getItem("id"), article_id);
+            // console.log(reviewers)
+
+            setReviewers(reviewers)
+            setArticleData({"title":"article1","content":"content1"}); 
+
+            setIsModalOpen(true); 
           } catch (error) {
             console.error('Error fetching article data:', error);
           }
@@ -49,6 +62,17 @@ const AssignReviewer = ()=>{
 
     const handleCancel = () => {
         setIsModalOpen(false);
+    }
+
+    const fetchAllReviewers = async (user_id, article_id)=>{
+      try {
+          // const response = await http.get(`/User/reviewersinfo/${articleId}`);
+          const response = await http.get(`/User/reviewersinfo`);
+          return response.data; 
+        } catch (error) {
+          console.error('Fetching reviewer data failed', error);
+          return null
+        }
     }
 
     const fetchArticleData = async (articleId) => {
@@ -143,6 +167,7 @@ const AssignReviewer = ()=>{
             onCancel={handleCancel}
             onOk={handleOk}
             articleData={articleData}
+            reviewers={reviewers}
         />
       </div>
     )
