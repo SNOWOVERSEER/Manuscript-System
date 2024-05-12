419f78afb350d7db7d7674b176d4dafa8dc49fe5
DonghongZhuang <50001468+DonghongZhuang@users.noreply.github.com>
2024-05-12 20:13:07 +1000

    editor review page

--- src/front_end/sila-frontend/src/apis/article.js
+++ src/front_end/sila-frontend/src/apis/article.js
@@ -1,8 +1,8 @@
 import { http } from "../utils";
 
 export function article_List_API(data_) {
-    return http({
-        url: `Manuscripts/submissions/${data_}`,
-        method: 'GET'
-    });
+  return http({
+    url: `Manuscripts/AuthorSubmissions/${data_}`,
+    method: "GET",
+  });
 }
